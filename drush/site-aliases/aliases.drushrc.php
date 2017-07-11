ad0966544d21a348add0943f575ab4f2a3cc8a80
Jayakrishnan Jayabal <jayakrishnan.jayabal@acquia.com>
2017-07-11 14:52:52 +0530

    BLT-000: Add new lightning sub profile and wrapper modules.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+
+// Local environment.
+$aliases['bltd8.local'] = array(
+  'root' => '/var/www/bltd8/docroot',
+  'uri' => 'http://local.bltd8.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['bltd8.local'] += array(
+    'remote-host' => 'local.bltd8.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
